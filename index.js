31f22a1b3146ec35a287976998664358a463243c
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-12 02:29:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ function whisper(string){
 
 function sayHiToGrandma(string)
 {
-  if(string === string.toUpperCase)
-  return "YES INDEED!"
-  else if(string === string.toLowerCase)
-  return "I can\'t hear you!"
+  if(string === string.toUpperCase)  return 'YES INDEED!'
+  else if(string === string.toLowerCase)  return 'I can\'t hear you!'
 }
 
 function logShout(string)

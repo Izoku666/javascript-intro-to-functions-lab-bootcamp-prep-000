fda9faff83dc337c25aa402d6ebf73d24c46d4da
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-12 02:30:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ function whisper(string){
 
 function sayHiToGrandma(string)
 {
-  if(string === string.toUpperCase)  return 'YES INDEED!'
-  else if(string === string.toLowerCase)  return 'I can\'t hear you!'
+  if(string === string.toUpperCase)  return '"YES INDEED!"'
+  else if(string === string.toLowerCase)  return '"I can\'t hear you!"'
+  else if(string ==='"I love you, Grandma."')
 }
 
 function logShout(string)
